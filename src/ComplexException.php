a543c6bb31228a09adc2ad65ea33edd68468ec2e
Grithin <grithin@clrmo.com>
2017-04-30 17:38:19 -0700

    conforming ComplexException to use getMessage with string

--- src/ComplexException.php
+++ src/ComplexException.php
@@ -8,20 +8,35 @@ Exception intended to accept an array as error details
 
 class ComplexException extends \Exception{
 	public $details = [];
-	public function __construct($message = null, $code = 0, \Exception $previous = null){
-		if(!is_scalar($message)){
-			$this->details = $message;
+	public function __construct($details = null, $code = 0, \Exception $previous = null){
+		# the message is extracted from the details, or it is a representation of the details
+		$message = null;
+		if(!Tool::is_scalar($details)){
+			if(is_array($details)){
+				if($details['message']){
+					$message = $details['message'];
+				}
+			}else{
+				if($details->message){
+					$message = $details->message;
+				}
+			}
+			if(!$message){
+				$message = Tool::flat_json_encode(Arrays::from($this->details));
+			}
+			$this->details = $details;
 		}else{
+			$message = $details;
 			$this->details = ['message'=>$message];
 		}
 
-		parent::__construct(\Grithin\Tool::flat_json_encode(array_merge(['message'=>''], Arrays::from($this->details) /*for ordering purposes*/) ), $code, $previous);
+		parent::__construct($message, $code, $previous);
 	}
 	public function getDetails(){
 		return $this->details;
 	}
 	# alias
 	public function get_details(){
-		return call_user_func_array([$this,'day_end'], func_get_args());
+		return call_user_func_array([$this,'getDetails'], func_get_args());
 	}
 }
